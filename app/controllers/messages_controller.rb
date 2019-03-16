c9798418754745c942d97377c88852a6fea8fceb
yw0920 <ywsep20@gmail.com>
2019-03-16 22:28:11 +0000

    indexアクション追加

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,27 @@
+class MessagesController < ApplicationController
+
+  def index
+    @messages = Message.all
+  end
+  
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+
+end
+
